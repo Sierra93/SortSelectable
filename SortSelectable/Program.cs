1738ad0bb4a7a50400e3e1311d1f89a4c74b882a
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2019-12-11 01:48:40 +0300

    Добавьте файлы проекта.

---
+++ SortSelectable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SortSelectable {
+    class Program {
+        static void Main() {
+            int min, temp;
+            int[] arr = { 10, 2, 6, 4, 89, 11, 12 };
+            for (int i = 0; i < arr.Length; i++) {
+                min = i;
+                for (int j = i + 1; j < arr.Length; j++) {
+                    if (arr[j] < arr[min]) {
+                        min = j;
+                    }
+                }
+                temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+            for (int i = 0; i < arr.Length; i++) {
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
